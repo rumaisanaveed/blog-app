156b0bb4f0c39db690405fe26d96eb1245097fec
Rumaisa Naveed <rumaisanaved@gmail.com>
2024-12-21 14:41:00 +0500

    feat: Added react portal for notification

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  // react portals
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
         <Main />
         <NextScript />
+        <div id="notifications"></div>
       </body>
     </Html>
   );
